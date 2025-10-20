fece217f1f344f155fcd830112e0adb9aa555daf
I.Crabtree <isaaclukecrabtree@gmail.com>
2025-10-20 22:59:57 +0100

    Redesigned background, added new sprites, added wall collision, simplified code,added new classes

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
 import pygame
 import sys
-from spriteclasses import Mushroom, Frog
+from sprites import Frog, Pond, Grass, Tree
+from player import Mushroom
 
 pygame.init()
 
-# Screen setup
 WIDTH, HEIGHT = 1920, 1080
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Mushroom Game")
 clock = pygame.time.Clock()
 
-# Game loop
-frog=Frog(1360,200)
+background = pygame.transform.scale(pygame.image.load("background.png").convert(), (WIDTH, HEIGHT))
+pond = Pond(1025,50 )
+frog = Frog(1360, 180)
+tree1 = Tree(1, 100)
+tree2 = Tree(800, 400)
+tree3 = Tree(1200, 700)
+tree4 = Tree(1, 600)
 mushroom = Mushroom(WIDTH, HEIGHT)
-mushroom.rect.center = (300,100)
+mushroom.rect.center = (300, 100)
+
 running = True
-
 while running:
-    clock.tick(60)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    mushroom.update()
+    mushroom.update(walls=[pond])
 
-    background = pygame.transform.scale(pygame.image.load("background.png"), (1920, 1080))
     screen.blit(background, (0, 0))
-    mushroom.draw(screen)
+    pond.draw(screen)
+    tree1.draw(screen)
+    tree2.draw(screen)
+    tree3.draw(screen)
+    tree4.draw(screen)
     frog.draw(screen)
+    mushroom.draw(screen)
 
     pygame.display.flip()
+    clock.tick(60)
 
 pygame.quit()
-sys.exit()
---
+++ sprites.py
@@ -0,0 +1,41 @@
+import pygame
+import random
+
+class Frog(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load("frogstill.png").convert_alpha(), (250, 250))
+        self.rect = self.image.get_rect(center=(x, y))
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+class Pond(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load("pond.png").convert_alpha(), (700, 400))
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+
+class Tree(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        size = random.randint(100, 400)
+        self.image = pygame.transform.scale(pygame.image.load("tree.png").convert_alpha(), (size, size))
+        self.rect = self.image.get_rect(topleft=(x, y))
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+class Grass(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load("windgrass1.png").convert_alpha(), (300, 300))
+        self.rect = self.image.get_rect(topleft=(x, y))
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
